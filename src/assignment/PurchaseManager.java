d82519dc2144a5cc4b29cb6f7a4f958fe6f8b7f6
RYAN\azu <blazerod123480@gmail.com>
2023-07-26 02:51:45 +0800

    LoginGUI

---
+++ src/assignment/PurchaseManager.java
@@ -0,0 +1,4 @@
+package assignment;
+public class PurchaseManager extends User{
+    
+}
---
+++ src/assignment/User.java
@@ -0,0 +1,10 @@
+package assignment;
+public class User {
+    private String username;
+    private String password;
+    private String role;
+    
+    public User(String username, String password, String role) {
+        
+    }
+}
